849640c39121b8dca4ecba694bdf405dceb1d599
Meetu Maltiar (mmaltiar) <mmaltiar@cisco.com>
2019-11-14 14:16:37 +0530

    Added exclude patterns for interceptor from properties file

--- src/main/java/com/cisco/cx/training/app/WebMvcConfig.java
+++ src/main/java/com/cisco/cx/training/app/WebMvcConfig.java
@@ -1,14 +1,17 @@
 package com.cisco.cx.training.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+	@Value("${cxpp.interceptor.exclude.path.patterns}")
+    public String excludePathPatterns ;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new RequestInterceptor()).excludePathPatterns("/v1/partner/training/ready", "/v1/partner/training/live");
+        registry.addInterceptor(new RequestInterceptor()).excludePathPatterns(excludePathPatterns);
     }
 }
