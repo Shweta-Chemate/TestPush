ab1831b0410e96c8bf5c13f55caa385a79cdf962
Meetu Maltiar (mmaltiar) <mmaltiar@cisco.com>
2019-11-14 10:04:42 +0530

    Added verification of UserId from Mashery Header

--- src/main/java/com/cisco/cx/training/app/RequestInterceptor.java
+++ src/main/java/com/cisco/cx/training/app/RequestInterceptor.java
@@ -7,29 +7,37 @@ import org.slf4j.MDC;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.cisco.cx.training.app.exception.BadRequestException;
 import com.cisco.cx.training.constants.Constants;
-import com.cisco.cx.training.models.User;
+import com.cisco.cx.training.models.MasheryUser;
 
 public class RequestInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String masheryHeader = request.getHeader(Constants.MASHERY_HANDSHAKE_HEADER_NAME);
 
-        try {
-            if (!StringUtils.isEmpty(masheryHeader)) {
-                MDC.put("userId", User.getInstance(masheryHeader).getCcoId());
-            } else {
-                throw new Exception("No Header Value");
-            }
-        } catch (Exception e) {
-            MDC.put("userId", "unknown-user");
-        }
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+		String masheryHeader = request.getHeader(Constants.MASHERY_HANDSHAKE_HEADER_NAME);
+		try {
+			if (!StringUtils.isEmpty(masheryHeader)) {
+				String userId = MasheryUser.getInstance(masheryHeader).getCcoId();
+				if (userId == null) {
+					throw new Exception("Logged in User is invalid");
+				} else {
+					MDC.put("userId", userId);
+				}
+			} else {
+				throw new Exception("Mashrey Header is mandatory");
+			}
+		} catch (Exception e) {
+			MDC.put("userId", "unknown-user");
+			throw new BadRequestException(e.getMessage());
 
-        return true;
-    }
+		}
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        MDC.clear();
-    }
+		return true;
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+		MDC.clear();
+	}
 }
---
+++ src/main/java/com/cisco/cx/training/models/MasheryUser.java
@@ -0,0 +1,64 @@
+package com.cisco.cx.training.models;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MasheryUser {
+	@SuppressWarnings("unused")
+	private static final Logger LOG = LoggerFactory.getLogger(MasheryUser.class);
+	private final static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+	// {"client_cco_id":"krsubbu","party_id":"52428","access_token_uid":"karbala2","access_token_accesslevel":"4"}
+	@JsonProperty("party_id")
+	private String partyId;
+
+	@JsonProperty("access_token_uid")
+	private String ccoId;
+
+	@JsonProperty("access_token_accesslevel")
+	private String accessLevel;
+
+	public String getPartyId() {
+		return this.partyId;
+	}
+
+	public String getCcoId() {
+		return this.ccoId;
+	}
+
+	public String getAccessLevel() {
+		return this.accessLevel;
+	}
+
+	private MasheryUser() {
+	}
+
+	public static MasheryUser getInstance(String requestHeader) {
+		MasheryUser masheryUser = null;
+
+		if (StringUtils.isNotBlank(requestHeader)) {
+			String decodeRequestHeader = new String(Base64.decodeBase64(requestHeader));
+			try {
+				masheryUser = OBJECT_MAPPER.readValue(decodeRequestHeader, MasheryUser.class);
+			} catch (Exception e) {
+				throw new IllegalArgumentException("Could not decode Mashery header, User unknown");
+			}
+		} else {
+			throw new IllegalArgumentException("Missing Mashery header, User unknown");
+		}
+
+		return masheryUser;
+	}
+
+	@Override
+	public String toString() {
+		return "MasheryUser {" + "partyId=" + partyId + ", ccoId=" + ccoId + ", accessLevel=" + accessLevel + '}';
+	}
+}
