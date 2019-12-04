ca6c447babb6fa58b583f10d4271c42f4a594448
Meetu Maltiar (mmaltiar) <mmaltiar@cisco.com>
2019-12-04 09:31:19 +0530

    Added include patterns

--- src/main/java/com/cisco/cx/training/app/WebMvcConfig.java
+++ src/main/java/com/cisco/cx/training/app/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.cisco.cx.training.app;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,11 +11,19 @@ import com.cisco.cx.training.constants.Constants;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 	@Value("${cxpp.interceptor.exclude.path.patterns}")
-    public String excludePathPatterns ;
+	public String excludePathPatterns;
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-    	 String[] excludePatterns = excludePathPatterns.split(Constants.COMMA.trim());
-        registry.addInterceptor(new RequestInterceptor()).excludePathPatterns(excludePatterns);
-    }
+	@Value("${cxpp.interceptor.include.path.patterns}")
+	public String includePathPatterns;
+
+	@Bean
+	public RequestInterceptor requestInterceptor() {
+		return new RequestInterceptor();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		String[] excludePatterns = excludePathPatterns.split(Constants.COMMA.trim());
+		registry.addInterceptor(requestInterceptor()).addPathPatterns(includePathPatterns).excludePathPatterns(excludePatterns);
+	}
 }
