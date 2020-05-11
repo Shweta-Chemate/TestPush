18ec3f1e238ce2f06ef7d2e985f03f50cc5a5074
Vimod Shankar (vimshank) <vimshank@cisco.com>
2020-05-11 21:26:49 +0530

    Adding MasheryObject

---
+++ src/main/java/com/cisco/cx/training/models/MasheryObject.java
@@ -0,0 +1,71 @@
+package com.cisco.cx.training.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MasheryObject {
+    @SuppressWarnings("unused")
+	private static final Logger LOG = LoggerFactory.getLogger(MasheryObject.class);
+    private final static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    //{"client_cco_id":"krsubbu","party_id":"52428","access_token_uid":"karbala2","access_token_accesslevel":"4"}
+    @JsonProperty("party_id")
+    private String partyId;
+
+    @JsonProperty("access_token_uid")
+    private String ccoId;
+
+    @JsonProperty("access_token_accesslevel")
+    private String accessLevel;
+
+    public String getPartyId() {
+        return this.partyId;
+    }
+
+    public String getCcoId() {
+        return this.ccoId;
+    }
+
+    public String getAccessLevel() {
+        return this.accessLevel;
+    }
+
+    public String getType() {
+        //return StringUtils.isNotBlank(this.accessLevel) ? CiscoProfileService.resolveUserType(this.accessLevel) : "Guest";
+        return null;
+    }
+
+    private MasheryObject() {}
+
+    public static MasheryObject getInstance(String requestHeader) {
+        MasheryObject masheryObject = null;
+
+        if (StringUtils.isNotBlank(requestHeader)) {
+            String decodeRequestHeader = new String(Base64.decodeBase64(requestHeader));
+            try {
+                masheryObject = OBJECT_MAPPER.readValue(decodeRequestHeader, MasheryObject.class);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Could not decode Mashery header, User unknown");
+            }
+        } else {
+            throw new IllegalArgumentException("Missing Mashery header, User unknown");
+        }
+
+        return masheryObject;
+    }
+
+    @Override
+    public String toString() {
+        return "MasheryUser {" +
+                "partyId=" + partyId +
+                ", ccoId=" + ccoId +
+                ", accessLevel=" + accessLevel +
+                '}';
+    }
+}
