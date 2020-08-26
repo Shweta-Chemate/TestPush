64489ca6f84e4acaa436256d06c8638829ca3662
Vimod Shankar <vimshank@cisco.com>
2020-08-26 23:52:17 +0530

    Update BookmarkDAOTest.java

--- src/test/java/com/cisco/cx/training/test/BookmarkDAOTest.java
+++ src/test/java/com/cisco/cx/training/test/BookmarkDAOTest.java
@@ -57,8 +57,7 @@ public class BookmarkDAOTest {
 	@Test(expected = GenericException.class)
 	public void getBookmarksError() throws IOException {
 		String entityId = "entityId";
-		String email = "email";
-		
+		String email = "email";		
 		SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
 		BoolQueryBuilder boolQuery = new BoolQueryBuilder();
 		QueryBuilder ccoIdQuery = QueryBuilders.matchPhraseQuery("email.keyword", email);
