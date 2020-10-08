b99bcbfd28a2273b821302a056be26244dd2701c
Sonali <sogandha@cisco.com>
2020-10-08 12:08:11 +0530

    TA532863 : Enable Swagger UI

--- src/main/java/com/cisco/cx/training/app/config/Swagger2Config.java
+++ src/main/java/com/cisco/cx/training/app/config/Swagger2Config.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -17,14 +17,9 @@ public class Swagger2Config {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-           .pathProvider(new RelativePathProvider(null) {
-            @Override
-            public String getApplicationBasePath() {
-                return "/training";
-            }
-        }).select()
-                .apis(RequestHandlerSelectors
-                .basePackage("com.cisco.cx.training.app"))
+        		.select()
+                .apis(RequestHandlerSelectors.basePackage("com.cisco.cx.training.app"))
+                .paths(PathSelectors.any())
                 .build().apiInfo(apiEndPointsInfo());
     }
     private ApiInfo apiEndPointsInfo() {
