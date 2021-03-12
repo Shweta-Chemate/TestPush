30b35d0b840adff7116fa1a2e3879a514b8730ef
Vimod Shankar <vimshank@cisco.com>
2021-03-12 16:26:08 +0530

    Update BookmarkDAOTest.java

--- src/test/java/com/cisco/cx/training/test/BookmarkDAOTest.java
+++ src/test/java/com/cisco/cx/training/test/BookmarkDAOTest.java
@@ -2,7 +2,6 @@ package com.cisco.cx.training.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
-
 import java.io.IOException;
 
 import org.elasticsearch.index.query.BoolQueryBuilder;
