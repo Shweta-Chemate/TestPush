f7d19b8675f2b9f69f01b63e816f965ae9654c29
Amruta Kulkarni <amrukul2@cisco.com>
2021-04-12 11:58:17 +0530

    formatting

--- src/main/java/com/cisco/cx/training/app/service/impl/LearningContentServiceImpl.java
+++ src/main/java/com/cisco/cx/training/app/service/impl/LearningContentServiceImpl.java
@@ -1,10 +1,8 @@
 package com.cisco.cx.training.app.service.impl;
-
 import org.springframework.stereotype.Service;
-
 import com.cisco.cx.training.app.service.LearningContentService;
 
 @Service
-public class LearningContentServiceImpl implements LearningContentService{
+public class LearningContentServiceImpl implements LearningContentService {
 
 }
