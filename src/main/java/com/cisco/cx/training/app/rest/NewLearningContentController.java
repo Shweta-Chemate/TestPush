b9fc74a9935fd5284cde72287cc3b9031f105afe
Amruta Kulkarni <amrukul2@cisco.com>
2021-05-04 13:31:46 +0530

    removing unused import

--- src/main/java/com/cisco/cx/training/app/rest/NewLearningContentController.java
+++ src/main/java/com/cisco/cx/training/app/rest/NewLearningContentController.java
@@ -10,7 +10,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
