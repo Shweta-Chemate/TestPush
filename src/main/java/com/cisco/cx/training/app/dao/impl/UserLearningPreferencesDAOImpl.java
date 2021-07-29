864e06bb51180b2d2a1ada592a5766392fb3fd48
sogandha <sogandha@cisco.com>
2021-07-29 10:27:28 +0530

    PXC-2743 Learning: Preferences

---
+++ src/main/java/com/cisco/cx/training/app/dao/impl/UserLearningPreferencesDAOImpl.java
@@ -0,0 +1,220 @@
+package com.cisco.cx.training.app.dao.impl;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClientBuilder;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest.Builder;
+import software.amazon.awssdk.services.dynamodb.model.PutItemResponse;
+import software.amazon.awssdk.services.dynamodb.model.QueryRequest;
+import software.amazon.awssdk.services.dynamodb.model.QueryResponse;
+
+import com.cisco.cx.training.app.config.PropertyConfiguration;
+import com.cisco.cx.training.app.dao.LearningBookmarkDAO;
+import com.cisco.cx.training.app.dao.ProductDocumentationDAO;
+import com.cisco.cx.training.app.dao.UserLearningPreferencesDAO;
+import com.cisco.cx.training.models.BookmarkResponseSchema;
+import com.cisco.cx.training.models.UserLearningPreference;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Repository
+public class UserLearningPreferencesDAOImpl implements UserLearningPreferencesDAO {
+	
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass().getName());
+	
+	@Autowired
+	private PropertyConfiguration propertyConfig;	
+	@Autowired
+	ProductDocumentationDAO productDocumentationDAO;
+	
+	private static final String USERID_SUFFIX = "";//_ulp
+	private static final String USERID_KEY="userid";	
+	private static enum PREFERENCES_KEYS {role,technology,language,region,timeinterval};
+	private static final String PREFERENCE_SUFFIX = "";//"_ulp";
+	private DynamoDbClient dbClient;	
+	private static ObjectMapper mapper = new ObjectMapper();
+	
+	public DynamoDbClient getDbClient() {
+		return dbClient;
+	}
+
+	public void setDbClient(DynamoDbClient dbClient) {
+		this.dbClient = dbClient;
+	}
+
+	@PostConstruct
+	public void init() //throws URISyntaxException
+	{
+		LOG.info("Initializing ULP for table :: {}", propertyConfig.getUlPreferencesTableName());
+		SdkHttpClient httpClient = ApacheHttpClient.builder().
+                connectionTimeout(Duration.ofSeconds(100))
+                .socketTimeout(Duration.ofSeconds(100))
+                .build();
+		
+		Region region = Region.of(propertyConfig.getAwsRegion());
+		DynamoDbClientBuilder dDbClientBuilder = DynamoDbClient.builder().httpClient(httpClient);
+				//.endpointOverride(new URI("http://localhost:8000"));
+		dDbClientBuilder.region(region);
+		dbClient = dDbClientBuilder.build();
+	}
+
+	@Override
+	public Map<String, List<UserLearningPreference>> createOrUpdateULP(String userId,
+			Map<String, List<UserLearningPreference>> ulPreferences) {		
+		LOG.info("Entering the createOrUpdateULP");
+		long requestStartTime = System.currentTimeMillis();		
+		Map<String, List<UserLearningPreference>> currentULPs = ulPreferences;//getULPs not required
+		if(currentULPs==null || currentULPs.isEmpty()) return ulPreferences;
+		Map<String, AttributeValue> itemValue = new HashMap<String, AttributeValue>();
+		itemValue.put(USERID_KEY, AttributeValue.builder().s(userId.concat(USERID_SUFFIX)).build());
+		Arrays.asList(PREFERENCES_KEYS.values()).forEach( preferenceKey ->{
+			if(ulPreferences.containsKey(preferenceKey.name()) )
+			{
+				Set<String> preferenceNames = new HashSet<String>();
+				List<UserLearningPreference> ulpList = ulPreferences.get(preferenceKey.name());
+				//LOG.info("preferenceKey {} ulpList {}",preferenceKey, ulpList);
+				if(ulpList!=null && !ulpList.isEmpty())
+				{
+					ulpList.forEach(up ->{
+						if(preferenceKey.equals(PREFERENCES_KEYS.timeinterval))
+						{
+								try {
+									String oneTI = new ObjectMapper().writeValueAsString(up.getTimeMap());
+									preferenceNames.add(oneTI);
+								} catch (JsonProcessingException e) {
+									LOG.warn("Invalid TI in create {} {}", up, e.getMessage());
+								}							
+							
+						}
+						else if(up.isSelected()) preferenceNames.add(up.getName());
+					});
+				}
+				
+				if(!preferenceNames.isEmpty())
+					itemValue.put(preferenceKey.name().concat(PREFERENCE_SUFFIX), AttributeValue.builder().ss(preferenceNames).build());
+			}					
+		});
+		LOG.info("Preprocessing done in {} ", (System.currentTimeMillis() - requestStartTime));
+		requestStartTime = System.currentTimeMillis();	
+		Builder putItemReq = PutItemRequest.builder();		
+		putItemReq.tableName(propertyConfig.getUlPreferencesTableName()).item(itemValue);
+		PutItemResponse response = dbClient.putItem(putItemReq.build());LOG.info("response {}",response);
+		LOG.info("response received in {} ", (System.currentTimeMillis() - requestStartTime));
+		if(response.sdkHttpResponse().isSuccessful()){		
+			return ulPreferences;
+		}else{
+			return null;
+		}
+	}
+
+	@Override
+	public Map<String, List<UserLearningPreference>> fetchUserLearningPreferences(String userId) {
+		LOG.info("Entering the fetch ULPs");
+		Map<String, List<UserLearningPreference>> ulpMap = new HashMap<String, List<UserLearningPreference>>();
+		ulpMap.putAll(getAllLatestPreferencesCategories());
+		long requestStartTime = System.currentTimeMillis();		
+		Map<String,String> expressionAttributesNames = new HashMap<>();
+		expressionAttributesNames.put("#userid",USERID_KEY);	    
+		Map<String,AttributeValue> expressionAttributeValues = new HashMap<>();	    
+		expressionAttributeValues.put(":useridValue",AttributeValue.builder().s(userId.concat(USERID_SUFFIX)).build());
+		QueryRequest queryRequest = QueryRequest.builder()
+				.tableName(propertyConfig.getUlPreferencesTableName())
+				.keyConditionExpression("#userid = :useridValue")
+				.expressionAttributeNames(expressionAttributesNames)
+				.expressionAttributeValues(expressionAttributeValues).build();
+		LOG.info("Preprocessing done in {} ", (System.currentTimeMillis() - requestStartTime));
+		requestStartTime = System.currentTimeMillis();	
+		QueryResponse queryResult = dbClient.query(queryRequest);
+		LOG.info("response received in {} ", (System.currentTimeMillis() - requestStartTime));
+		requestStartTime = System.currentTimeMillis();	
+		List<Map<String,AttributeValue>> attributeValues = queryResult.items();		
+		//LOG.info("attributeValues {} , {}", attributeValues, attributeValues.size());
+		if(attributeValues.size()>0) {
+			Map<String,AttributeValue> userLearningPreferences = attributeValues.get(0);
+			Arrays.asList(PREFERENCES_KEYS.values()).forEach( preferenceKey ->{
+				AttributeValue preferenceSet = userLearningPreferences.get(preferenceKey.name());
+				if(preferenceSet != null)
+				{
+					Set<String> ulps = new HashSet<String>(preferenceSet.ss());	
+					//LOG.info(" preferenceKey {} ulps {}",preferenceKey, ulps);
+					if(preferenceKey.equals(PREFERENCES_KEYS.timeinterval))
+					{
+						List<UserLearningPreference> listTI = new ArrayList<UserLearningPreference>();
+						ulps.forEach(timeInterval -> {							
+							try {
+								UserLearningPreference timeULP = new UserLearningPreference();
+								timeULP.setSelected(true);
+								timeULP.setTimeMap(new ObjectMapper().readValue(timeInterval, Map.class));
+								listTI.add(timeULP);
+							} catch (JsonProcessingException e) {
+								LOG.warn("Invalid TimeInterval preference {} err={}",timeInterval, e.getMessage());								
+							}						
+						});	
+						ulpMap.put(PREFERENCES_KEYS.timeinterval.name(), listTI);
+					}
+					else
+					{
+						List<UserLearningPreference> preDBList = ulpMap.get(preferenceKey.name());					
+						if(preDBList!=null && !preDBList.isEmpty())
+							preDBList.forEach(up -> {
+								if(ulps.contains(up.getName())) up.setSelected(true);
+							});
+					}					
+				}			
+			});			
+		}
+		LOG.info("final response in {}", (System.currentTimeMillis() - requestStartTime));
+		return ulpMap;
+	}
+
+	private Map<? extends String, ? extends List<UserLearningPreference>> getAllLatestPreferencesCategories() {
+		Map<String, List<UserLearningPreference>> dbMap = new HashMap<String, List<UserLearningPreference>>();
+		List<String> roles = productDocumentationDAO.getAllRolesForPreferences();
+		dbMap.put(PREFERENCES_KEYS.role.name(),setULP(roles));
+		List<String> technologies = productDocumentationDAO.getAllTechnologyForPreferences();
+		dbMap.put(PREFERENCES_KEYS.technology.name(),setULP(technologies));
+		List<String> regions = productDocumentationDAO.getAllRegionForPreferences();
+		dbMap.put(PREFERENCES_KEYS.region.name(),setULP(regions));	
+		List<String> languages = productDocumentationDAO.getAllLanguagesForPreferences();
+		dbMap.put(PREFERENCES_KEYS.language.name(),setULP(languages));	
+		return dbMap;
+	}
+	
+	private List<UserLearningPreference> setULP(List<String> dbPrefList)
+	{
+		List<UserLearningPreference> dbPrefs = new ArrayList<UserLearningPreference>();
+		dbPrefList.forEach(dbPref -> {
+			UserLearningPreference ulp= new UserLearningPreference();
+			ulp.setName(dbPref);ulp.setSelected(false);
+			dbPrefs.add(ulp);
+		});
+		return dbPrefs;
+	}
+}
