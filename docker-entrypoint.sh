47fef7a0f0a2851777968b8df61bb27a714c3c71
Meetu Maltiar <mmaltiar@isco.com>
2021-12-15 17:16:57 +0530

    APPD fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 #Appd configuration
 if [ ! -z "$APPDYNAMICS_ENABLED" ]; then
-APPD_CONF="-javaagent:/AppServerAgent/javaagent.jar -Dappdynamics.agent.uniqueHostId=$(sed -rn '1s#./##; 1s/(.{12})./\1/p' /proc/self/cgroup) ${APPDYNAMICS_JVM_PARAMETER}"
+APPD_CONF="-javaagent:/AppServerAgent/javaagent.jar -Dappdynamics.agent.uniqueHostId=$(sed -rn '1s#.*/##; 1s/(.{12}).*/\1/p' /proc/self/cgroup) ${APPDYNAMICS_JVM_PARAMETER}"
 fi
 
 PROXY_SETTING="-Dhttp.proxyHost=${HTTP_PROXY} -Dhttp.proxyPort=${HTTP_PORT} -Dhttps.proxyHost=${HTTPS_PROXY} -Dhttps.proxyPort=${HTTPS_PORT} -Dhttp.nonProxyHosts=${NON_PROXY}"
